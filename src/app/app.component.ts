cb25630605b41fe922dff880eb3ca7c19911c46c
Chr1stiani <the.legendary.christian@gmail.com>
2024-05-08 14:10:28 +0200

    working copy of a project with Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    LandingPageComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
